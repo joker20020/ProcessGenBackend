aae3ff13c0a7767471ef49a7f56e9389915a6994
joker20020 <405997563@qq.com>
2026-07-09 18:09:48 +0800

    feat(qt): ApiWorker thread-pool worker

---
+++ qt_tool/workers.py
@@ -0,0 +1,30 @@
+from PySide6.QtCore import QObject, QRunnable, Signal
+
+
+class _Signals(QObject):
+    finished = Signal(object)
+    failed = Signal(str)
+
+
+class ApiWorker(QRunnable):
+    def __init__(self, fn, *args, **kwargs):
+        super().__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = _Signals()
+
+    @property
+    def finished(self):
+        return self.signals.finished
+
+    @property
+    def failed(self):
+        return self.signals.failed
+
+    def run(self):
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+            self.signals.finished.emit(result)
+        except Exception as e:
+            self.signals.failed.emit(str(e))
